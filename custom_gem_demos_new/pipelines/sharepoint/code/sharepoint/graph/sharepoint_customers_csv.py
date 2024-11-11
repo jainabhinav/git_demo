f7f1e7557982f2fe0fe328ce284ccde9b4e07b07
abhinav@simpledatalabs.com <abhinav@simpledatalabs.com>
2024-11-11 11:39:28 +0000

    Enhance SharePoint pipeline with SFTP support and update customer_catalog_scd2 description

--- custom_gem_demos_new/pipelines/sharepoint/code/sharepoint/graph/sharepoint_customers_csv.py
+++ custom_gem_demos_new/pipelines/sharepoint/code/sharepoint/graph/sharepoint_customers_csv.py
@@ -7,8 +7,34 @@ from sharepoint.config.ConfigStore import *
 from sharepoint.functions import *
 
 def sharepoint_customers_csv(spark: SparkSession) -> DataFrame:
+    import paramiko
+    import os
+
+    def download_file_from_sftp(username, password, host, remote_file_path, local_file_path):
+        ssh_client = paramiko.SSHClient()
+        ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+        try:
+            ssh_client.connect(host, username = username, password = password)
+            sftp_client = ssh_client.open_sftp()
+
+            try:
+                ssh_client.open_sftp().get(remote_file_path, local_file_path)
+            finally:
+                ssh_client.open_sftp().close()
+        finally:
+            ssh_client.close()
+
+    download_file_from_sftp(
+        username = f"{Config.sftpusername}",
+        password = f"{Config.sftppassword}",
+        host = "34.42.38.165",
+        remote_file_path = "/uploads/customers.csv",
+        local_file_path = os.path.join("/tmp", os.path.basename("/uploads/customers.csv"))
+    )
+
     return spark.read\
         .option("header", True)\
         .option("inferSchema", True)\
         .option("sep", ",")\
-        .csv("/personal/rakesh_prophecy340_onmicrosoft_com/Documents/customers.csv")
+        .csv("file://{}".format(os.path.join("/tmp", os.path.basename("/uploads/customers.csv"))))
